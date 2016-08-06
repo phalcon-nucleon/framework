9c9ba58ffca33995ce57d9890f5473cbec4a6926
guial <gui.allegret@gmail.com>
2016-08-07 00:58:49 +0200

    Throttle Middleware 1.0.0-alpha
    + Throttling Logic.

--- src/Luxury/Http/Middleware/Throttle.php
+++ src/Luxury/Http/Middleware/Throttle.php
@@ -3,9 +3,9 @@
 namespace Luxury\Http\Middleware;
 
 use Luxury\Constants\Events as EventSpaces;
+use Luxury\Constants\Services;
 use Luxury\Foundation\Middleware\Controller as ControllerMiddleware;
 use Luxury\Middleware\BeforeMiddleware;
-use Luxury\Support\Facades\Log;
 
 /**
  * Class Throttle
@@ -14,6 +14,20 @@ use Luxury\Support\Facades\Log;
  */
 class Throttle extends ControllerMiddleware implements BeforeMiddleware
 {
+    /**
+     * Number of max request by $decay
+     *
+     * @var int
+     */
+    private $max;
+
+    /**
+     * TODO
+     *
+     * @var int
+     */
+    private $decay;
+
     /**
      * Called before the execution of handler
      *
@@ -26,7 +40,59 @@ class Throttle extends ControllerMiddleware implements BeforeMiddleware
      */
     public function before($event, $source, $data = null)
     {
-        // TODO Implement Rate Limit
-        Log::debug(__METHOD__);
+        $signature = $this->resolveRequestSignature();
+
+        /** @var \Phalcon\Cache\BackendInterface $cache */
+        $cache = $this->{Services::CACHE};
+
+        $attempts = $cache->get($signature);
+
+        $attempts = intval($attempts);
+
+        if ($attempts > $this->max) {
+            $this->buildResponse($attempts, true);
+            // TODO
+            throw new \Exception;
+        }
+
+        $attempts++;
+
+        $this->buildResponse($attempts, false);
+
+        $cache->save($signature, $attempts, $this->decay * 60);
+    }
+
+    /**
+     * Resolve the request signature based on :
+     *  HOST . URI . ClientIP
+     *
+     * @return string
+     */
+    private function resolveRequestSignature()
+    {
+        $request = $this->request;
+        $router  = $this->router;
+
+        return sha1(
+            $router->getModuleName() .
+            ':' . $router->getNamespaceName() .
+            ':' . $router->getControllerName() .
+            ':' . $router->getActionName() .
+            '|' . $request->getHttpHost() .
+            '|' . $request->getURI() .
+            '|' . $request->getClientAddress()
+        );
+    }
+
+    private function buildResponse($attempts, $retry = null)
+    {
+        $response = $this->response;
+
+        $response->setHeader('X-RateLimit-Limit', $this->max);
+        $response->setHeader('X-RateLimit-Remaining', $this->max - $attempts);
+
+        if (!is_null($retry)) {
+            $response->setHeader('Retry-After', $this->decay);
+        }
     }
 }

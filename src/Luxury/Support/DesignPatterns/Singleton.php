470272fd9242359c420147ba92232e6ef7aacdc5
Ark4ne <gui.allegret@gmail.com>
2016-10-13 21:09:14 +0200

    Add Design Patterns classes

---
+++ src/Luxury/Support/DesignPatterns/Singleton.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Luxury\Support\DesignPatterns;
+
+/**
+ * Class Singleton
+ *
+ * @package  Luxury\Support\DesignPatterns
+ */
+abstract class Singleton
+{
+    /**
+     * @var static
+     */
+    private static $instance;
+
+    /**
+     * Singleton constructor.
+     */
+    protected function __construct()
+    {
+    }
+
+    /**
+     * @throws \RuntimeException
+     */
+    final private function __clone()
+    {
+        throw new \RuntimeException('Try to clone Singleton instance.');
+    }
+
+    /**
+     * Instantiate & return static instance
+     *
+     * @return static
+     */
+    public static function instance()
+    {
+        if (self::$instance == null) {
+            self::$instance = new static();
+        }
+
+        return self::$instance;
+    }
+}

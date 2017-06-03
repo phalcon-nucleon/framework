20df7368a421dd613a6e1fe2fa31f428ff1fbb94
Ark4ne <gui.allegret@gmail.com>
2017-06-03 03:21:36 +0200

    feat(Cli\Router): Router::addTask change $pattern by $command. Use "my:cmd {arg}" for declaration insteadof regex.

--- src/Neutrino/Cli/Router.php
+++ src/Neutrino/Cli/Router.php
@@ -14,27 +14,27 @@ class Router extends \Phalcon\Cli\Router
      *
      * ex : $router->addTask('some', SomeTask::class);
      *
-     * @param string      $pattern
+     * @param string      $command
      * @param string      $class
      * @param string|null $action
      * @param array       $params
      *
-     * @return \Phalcon\Cli\Router\Route
+     * @return \Phalcon\Cli\Router\Route|\Phalcon\Cli\Router\RouteInterface
      */
-    public function addTask($pattern, $class, $action = null, array $params = [])
+    public function addTask($command, $class, $action = null, array $params = [])
     {
         $params['task'] = $class;
 
         $params['action'] = $action;
 
-        preg_match_all('/:([\w_]+):/', $pattern, $matches);
+        preg_match_all('/\{([\w_]+)\}/', $command, $matches);
 
         foreach ($matches[0] as $k => $match) {
-            $pattern = str_replace($match, '([[:alnum:]]+)', $pattern);
+            $command = str_replace($match, '([[:alnum:]]+)', $command);
 
             $params[str_replace(':', '', $match)] = $k + 1;
         }
 
-        return $this->add($pattern, $params);
+        return $this->add($command, $params);
     }
 }

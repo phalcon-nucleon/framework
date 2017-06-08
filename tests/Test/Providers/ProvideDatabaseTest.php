82914325321b6d6dd9a1ebb183f67d1f17206635
Ark4ne <gui.allegret@gmail.com>
2017-06-08 15:13:28 +0200

    Providers\Database Unit test

--- tests/Test/Providers/ProvideDatabaseTest.php
+++ tests/Test/Providers/ProvideDatabaseTest.php
@@ -4,6 +4,7 @@ namespace Test\Providers;
 
 use Fake\Kernels\Http\StubKernelHttpEmpty;
 use Neutrino\Constants\Services;
+use Neutrino\Database\DatabaseStrategy;
 use Neutrino\Providers\Database;
 use Test\TestCase\TestCase;
 
@@ -14,6 +15,21 @@ use Test\TestCase\TestCase;
  */
 class ProvideDatabaseTest extends TestCase
 {
+    public static function setUpBeforeClass()
+    {
+        parent::setUpBeforeClass();
+
+        fclose(fopen(__DIR__ . '/fake.sqlite', 'w'));
+    }
+
+    public static function tearDownAfterClass()
+    {
+        parent::tearDownAfterClass();
+
+        if (file_exists(__DIR__ . '/fake.sqlite')) {
+            unlink(__DIR__ . '/fake.sqlite');
+        }
+    }
 
     public function setUp()
     {
@@ -26,7 +42,7 @@ class ProvideDatabaseTest extends TestCase
                     'sqlite' => [
                         'adapter' => \Phalcon\Db\Adapter\Pdo\Sqlite::class,
                         'options' => [
-                            'dbname' => 'dbname'
+                            'dbname' => __DIR__ . '/fake.sqlite'
                         ]
                     ]
                 ],
@@ -41,9 +57,6 @@ class ProvideDatabaseTest extends TestCase
         return StubKernelHttpEmpty::class;
     }
 
-    /**
-     * @expectedException \Phalcon\Db\Exception
-     */
     public function testRegister()
     {
         $provider = new Database();
@@ -51,12 +64,45 @@ class ProvideDatabaseTest extends TestCase
         $provider->registering();
 
         $this->assertTrue($this->getDI()->has(Services::DB));
-
+        $this->assertTrue($this->getDI()->has(Services::DB . '.sqlite'));
         $this->assertTrue($this->getDI()->getService(Services::DB)->isShared());
+        $this->assertTrue($this->getDI()->getService(Services::DB . '.sqlite')->isShared());
+        $this->assertEquals($this->getDI()->getRaw(Services::DB), $this->getDI()->getRaw(Services::DB . '.sqlite'));
+    }
 
-        $this->assertInstanceOf(
-            \Phalcon\Db\Adapter\Pdo\Sqlite::class,
-            $this->getDI()->getShared(Services::DB)
-        );
+    public function testRegisterMultiConnection()
+    {
+        $this->mockService(Services::CONFIG, new \Phalcon\Config([
+            'database' => [
+                'default'     => 'sqlite',
+                'connections' => [
+                    'sqlite'  => [
+                        'adapter' => \Phalcon\Db\Adapter\Pdo\Sqlite::class,
+                        'options' => [
+                            'dbname' => __DIR__ . '/fake.sqlite'
+                        ]
+                    ],
+                    'sqlite2' => [
+                        'adapter' => \Phalcon\Db\Adapter\Pdo\Sqlite::class,
+                        'options' => [
+                            'dbname' => __DIR__ . '/fake.sqlite'
+                        ]
+                    ]
+                ],
+            ]
+        ]), true);
+
+        $provider = new Database();
+
+        $provider->registering();
+
+        $this->assertTrue($this->getDI()->has(Services::DB));
+        $this->assertInstanceOf(DatabaseStrategy::class, $this->getDI()->getShared(Services::DB));
+
+        foreach (['sqlite', 'sqlite2'] as $service) {
+            $this->assertTrue($this->getDI()->has(Services::DB . '.' . $service));
+            $this->assertTrue($this->getDI()->getService(Services::DB)->isShared());
+            $this->assertTrue($this->getDI()->getService(Services::DB . '.' . $service)->isShared());
+        }
     }
 }

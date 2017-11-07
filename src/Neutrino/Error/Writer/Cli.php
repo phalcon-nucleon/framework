8ce464c2ba8e01febd2a72a33ab3e4dbf480d3bd
Ark4ne <gui.allegret@gmail.com>
2017-11-07 17:22:33 +0100

    feat(Error\Writer\Cli): Improve cli error handler output

--- src/Neutrino/Error/Writer/Cli.php
+++ src/Neutrino/Error/Writer/Cli.php
@@ -7,6 +7,7 @@ use Neutrino\Constants\Services;
 use Neutrino\Error\Error;
 use Neutrino\Error\Helper;
 use Phalcon\Di;
+use Phalcon\Logger as PhalconLogger;
 
 /**
  * Class Cli
@@ -17,20 +18,44 @@ class Cli implements Writable
 {
     public function handle(Error $error)
     {
-        if (!$error->isFateful()) {
-            return;
-        }
-
         $di = Di::getDefault();
 
         if ($di && $di->has(Services\Cli::OUTPUT)) {
             $output = $di->getShared(Services\Cli::OUTPUT);
 
-            $block = new Block($output, 'warn', ['padding' => 4]);
+            $block = new Block($output, $this->getColoration($error), ['padding' => 4]);
 
             $block->draw(explode("\n", Helper::format($error)));
         } else {
             echo Helper::format($error);
+        }
+    }
+
+    /**
+     * @param \Neutrino\Error\Error $error
+     *
+     * @return string
+     */
+    protected function getColoration(Error $error)
+    {
+        if($error->isException){
+            return 'error';
+        }
+
+        switch (Helper::getLogType($error->code)){
+            case PhalconLogger::ALERT:
+            case PhalconLogger::CRITICAL:
+            case PhalconLogger::EMERGENCY:
+            case PhalconLogger::ERROR:
+                return 'error';
+            case PhalconLogger::WARNING:
+                return 'warn';
+            case PhalconLogger::NOTICE:
+                return 'notice';
+            case PhalconLogger::INFO:
+                return 'info';
+            default:
+                return 'error';
         }
     }
 }

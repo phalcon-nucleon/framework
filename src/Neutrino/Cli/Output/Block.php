0c62ded7081b885ecee0115c63aef548e5df72df
Ark4ne <gui.allegret@gmail.com>
2018-02-02 00:29:24 +0100

    feat(Cli\Output\Block): Limit block length

--- src/Neutrino/Cli/Output/Block.php
+++ src/Neutrino/Cli/Output/Block.php
@@ -34,8 +34,19 @@ class Block
     public function draw($lines = [])
     {
         $maxlen = 0;
+        $rows = [];
+
         foreach ($lines as $line) {
-            $maxlen = max($maxlen, strlen($line));
+            $len = strlen($line);
+
+            if ($len > 100) {
+                $parts = str_split($line, 100);
+                $rows = array_merge($rows, $parts);
+                $maxlen = max($maxlen, 100);
+            } else {
+                $maxlen = max($maxlen, $len);
+                $rows[] = $line;
+            }
         }
 
         $padding = Arr::get($this->options, 'padding', 4);
@@ -44,7 +55,7 @@ class Block
 
         $pad = str_repeat(' ', $padding / 2);
 
-        foreach ($lines as $line) {
+        foreach ($rows as $line) {
             $this->output->{$this->style}($pad . str_pad($line, $maxlen, ' ', STR_PAD_RIGHT) . $pad);
         }
 

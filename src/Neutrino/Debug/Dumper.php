ba26aacd5a2302b1ce5a249710bbcdc4e3fbecbf
Ark4ne <gui.allegret@gmail.com>
2018-03-13 23:30:02 +0100

    feat(Debug\Dump): Improve array, Recursion, and display

--- src/Neutrino/Debug/Dumper.php
+++ src/Neutrino/Debug/Dumper.php
@@ -35,15 +35,16 @@ class Dumper
             return $dump;
         }
 
-        if (is_array($var) && in_array($var, $this->dumped, true)) {
-            $dump = '<code  class="nuc-' . gettype($var) . '">';
-            $dump .= 'array</code> *RECURSION*';
+        $id = $this->objId($var);
+
+        if (is_array($var) && isset($this->dumpRefs[$id])) {
+            $dump = '<code  class="nuc-array">';
+            $dump .= 'array:' . count($var) . ' </code> <span class="nuc-closure">[</span><span class="nuc-toggle nuc-toggle-array" data-target="nuc-ref-' . $id . '">#' . $id . '</span>';
+            $dump .= '<span class="nuc-closure nuc-close">]</span>';
             $this->lvl--;
 
             return $dump;
         }
-
-        $id = $this->objId($var);
 
         if (is_object($var) && isset($this->dumpRefs[$id])) {
             $dump = '<code class="nuc-' . gettype($var) . '">';
@@ -66,11 +67,11 @@ class Dumper
         } elseif (is_resource($var)) {
             $dump = '<code class="nuc-resource">resource</code>';
         } elseif (is_array($var)) {
-            $this->dumped[] = $var;
             $dump = '<code class="nuc-array">array:' . count($var) . '</code> <span class="nuc-closure">[</span>';
-            if (!empty($var)) {
-                $dump .= '<span class="nuc-toggle nuc-toggle-array"></span>';
-                $dump .= '<ul class="nuc-array">';
+            if(!empty($var)){
+                $this->dumpRefs[$id] = true;
+                $dump .= '<span class="nuc-toggle nuc-toggle-array" data-target="nuc-ref-' . $id . '">#' . $id . '</span>';
+                $dump .= '<ul class="nuc-array" id="nuc-ref-' . $id . '">';
                 foreach ($var as $key => $val) {
                     $dump .= '<li class="nuc-' . gettype($val) . ' ' . (is_array($val) || is_object($val) ? 'nuc-close' : '') . '">';
                     $dump .= $this->__dump($key) . ' <span class="nuc-sep">=></span> ';
@@ -80,8 +81,6 @@ class Dumper
                 $dump .= '</ul>';
             }
             $dump .= '<span class="nuc-closure nuc-close">]</span>';
-
-            array_pop($this->dumped);
         } elseif (is_object($var)) {
             $this->dumpRefs[$id] = true;
 
@@ -142,17 +141,21 @@ class Dumper
 
     private function objId($var)
     {
-        if (!is_object($var)) {
-            return null;
+        if (is_object($var)) {
+            $hash = spl_object_hash($var);
+        } elseif (is_array($var)) {
+            $hash = md5(@var_export($var, true));
         }
 
-        $spl = spl_object_hash($var);
+        if (isset($hash)) {
+            if (isset($this->hash[$hash])) {
+                return $this->hash[$hash];
+            }
 
-        if (isset($this->hash[$spl])) {
-            return $this->hash[$spl];
+            return $this->hash[$hash] = self::uid();
         }
 
-        return $this->hash[$spl] = self::uid();
+        return null;
     }
 
     private static function uid()
@@ -202,7 +205,7 @@ class Dumper
 
         $outputted = true;
 
-        echo '<style>pre.nuc-dump{margin:0 0 5px;padding:5px;background:#232525;color:#eee;line-height:1.5;font:12px monospace;text-align:left;word-wrap:break-word;white-space:pre-wrap;word-break:break-all;position:relative;z-index:99999}pre.nuc-dump code,pre.nuc-dump code.nuc-key{color:#a69730}pre.nuc-dump ul{margin:0;padding:0;list-style-type:none;position:relative}pre.nuc-dump ul::before{content:" ";display:block;position:absolute;width:0;top:0;bottom:0;left:2px;border-left:1px dotted rgba(255,255,255,.15)}pre.nuc-dump ul li{margin:0 0 0 15px;padding:0;list-style-type:none}pre.nuc-dump small{font-size:80%}pre.nuc-dump li.nuc-close>ul{display:none}pre.nuc-dump li.nuc-open>ul{display:inherit}pre.nuc-dump .nuc-toggle{padding:0 2px;cursor:pointer;color:#919292;}pre.nuc-dump .nuc-toggle:hover{color:#fefefe;}pre.nuc-dump .nuc-open .nuc-toggle::after{font:10px sans-serif;content:"▼"}pre.nuc-dump .nuc-close .nuc-toggle::after{font:10px sans-serif;content:"►"}pre.nuc-dump .nuc-toggle.nuc-toggle-object{border-radius:2px}pre.nuc-dump .nuc-toggle.nuc-toggle-object:hover{background:rgba(255,255,255,.2);border-radius:2px}pre.nuc-dump .nuc-open .nuc-toggle.nuc-toggle-object::after{content:" ▼"}pre.nuc-dump .nuc-close .nuc-toggle.nuc-toggle-object::after{content:" ►"}pre.nuc-dump .nuc-modifier{color:#c16b2a}pre.nuc-dump code.nuc-const{color:#CC7832}pre.nuc-dump code.nuc-double,pre.nuc-dump code.nuc-float,pre.nuc-dump code.nuc-integer{color:#90caf9}pre.nuc-dump code.nuc-string{color:#52b33b}pre.nuc-dump code.nuc-string.nuc-truncate{cursor:pointer}pre.nuc-dump .nuc-closure,pre.nuc-dump .nuc-sep{color:#ef6c00}pre.nuc-dump code.nuc-string.nuc-truncate::after{color:#d800ff;font-weight:700;line-height:11px;content:\' >\'}pre.nuc-dump code.nuc-string.nuc-truncate.nuc-open::after{content:\' <\'}pre.nuc-dump code.nuc-array{color:#CC7832}pre.nuc-dump code.nuc-object{color:#00b0ff}</style>';
-        echo '<script>window.nucDumper=window.nucDumper||function(f){function e(a,b){if(a===b)return!1;var c=a.parentElement;return c===b?!0:c?e(c,b):!1}function d(a){a&&a.querySelector("ul")&&(a.classList.toggle("nuc-close"),a.classList.toggle("nuc-open"))}function g(a){a=a.target;var b=a.tagName,c=a.classList;"CODE"===a.tagName&&c.contains("nuc-truncate")?(c.toggle("nuc-open"),a.innerText=c.contains("nuc-open")?a.dataset.a:a.dataset.a.substr(0,117)):"SPAN"===b&&a.hasAttribute("data-target")?(b=f.getElementById(a.getAttribute("data-target")),e(a,b)||(b.parentNode===a.parentNode?d(b.parentElement):((b=b.parentElement)&&b.querySelector("ul")&&(b.classList.add("nuc-close"),b.classList.remove("nuc-open")),a.parentNode.insertBefore(f.getElementById(a.getAttribute("data-target")),a.nextSibling),d(a.parentElement)))):"SPAN"===b&&c.contains("nuc-toggle")&&(a=a.parentElement,"LI"===a.tagName&&d(a))}return function(a){a=f.getElementById(a);for(var b=a.querySelectorAll("code.nuc-string"),c,d=0,e=b.length;d<e;d++)c=b[d],120<c.innerText.length&&(c.classList.add("nuc-truncate"),c.dataset.a=c.innerText,c.innerText=c.innerText.substr(0,117));a.addEventListener("click",g)}}(document);</script>';
+        echo '<style>pre.nuc-dump{margin:0 0 5px;padding:5px;background:#232525;color:#eee;line-height:1.5;font:12px monospace;text-align:left;word-wrap:break-word;white-space:pre-wrap;word-break:break-all;position:relative;z-index:99999}pre.nuc-dump code,pre.nuc-dump code.nuc-key{color:#a69730}pre.nuc-dump ul{margin:0;padding:0;list-style-type:none;position:relative}pre.nuc-dump ul::before{content:" ";display:block;position:absolute;width:0;top:0;bottom:0;left:2px;border-left:1px dotted rgba(255,255,255,.15)}pre.nuc-dump ul li{margin:0 0 0 15px;padding:0;list-style-type:none}pre.nuc-dump small{font-size:80%}pre.nuc-dump li.nuc-close>ul{display:none}pre.nuc-dump li.nuc-open>ul{display:inherit}pre.nuc-dump .nuc-toggle{padding:0 2px;cursor:pointer;color:#919292;border-radius:2px}pre.nuc-dump .nuc-toggle:hover{color:#fefefe}pre.nuc-dump .nuc-open .nuc-toggle::after{font:10px sans-serif;content:" ▼"}pre.nuc-dump .nuc-close .nuc-toggle::after{font:10px sans-serif;content:" ►"}pre.nuc-dump .nuc-parent:after{content:""!important}pre.nuc-dump .nuc-toggle-object:hover{background:rgba(255,255,255,.2)}pre.nuc-dump .nuc-hover{background:#8b18a7!important;color:#fefefe!important}pre.nuc-dump .nuc-modifier{color:#c16b2a}pre.nuc-dump code.nuc-const{color:#CC7832}pre.nuc-dump code.nuc-double,pre.nuc-dump code.nuc-float,pre.nuc-dump code.nuc-integer{color:#90caf9}pre.nuc-dump code.nuc-string{color:#52b33b}pre.nuc-dump code.nuc-string.nuc-truncate{cursor:pointer}pre.nuc-dump .nuc-closure,pre.nuc-dump .nuc-sep{color:#ef6c00}pre.nuc-dump code.nuc-string.nuc-truncate::after{color:#d800ff;font-weight:700;line-height:11px;content:\' >\'}pre.nuc-dump code.nuc-string.nuc-truncate.nuc-open::after{content:\' <\'}pre.nuc-dump code.nuc-array{color:#CC7832}pre.nuc-dump code.nuc-object{color:#00b0ff}</style>';
+        echo '<script>window.nucDumper=window.nucDumper||function(f){function g(a,b){var c=a.parentNode;return c.id===b?!0:"PRE"===c.tagName?!1:g(c,b)}function h(a){var b;var c=a.querySelectorAll(".nuc-parent");var d=0;for(b=c.length;d<b;d++)c[d].classList.remove("nuc-parent");c=a.querySelectorAll("[data-target]");d=0;for(b=c.length;d<b;d++)a=c[d],g(a,a.dataset.target)&&a.classList.add("nuc-parent")}function k(a,b){if(a===b)return!1;var c=a.parentElement;return c===b?!0:c?k(c,b):!1}function e(a){a&&a.querySelector("ul")&&(a.classList.toggle("nuc-close"),a.classList.toggle("nuc-open"))}function l(a){a=a.target;var b=a.tagName,c=a.classList;"CODE"===a.tagName&&c.contains("nuc-truncate")?(c.toggle("nuc-open"),a.innerText=c.contains("nuc-open")?a.dataset.a:a.dataset.a.substr(0,117)):"SPAN"===b&&a.hasAttribute("data-target")?(b=f.getElementById(a.getAttribute("data-target")),k(a,b)||(b.parentNode===a.parentNode?e(b.parentElement):((b=b.parentElement)&&b.querySelector("ul")&&(b.classList.add("nuc-close"),b.classList.remove("nuc-open")),a.parentNode.insertBefore(f.getElementById(a.getAttribute("data-target")),a.nextSibling),e(a.parentElement),h(this)))):"SPAN"===b&&c.contains("nuc-toggle")&&(a=a.parentElement,"LI"===a.tagName&&e(a))}return function(a){a=f.getElementById(a);for(var b=a.querySelectorAll("code.nuc-string"),c,d=0,e=b.length;d<e;d++)c=b[d],120<c.innerText.length&&(c.classList.add("nuc-truncate"),c.dataset.a=c.innerText,c.innerText=c.innerText.substr(0,117));a.addEventListener("click",l);a.addEventListener("mouseover",function(a){a=a.target;a.classList.contains("nuc-parent")&&(a.classList.add("nuc-hover"),document.getElementById(a.dataset.target).previousElementSibling.classList.add("nuc-hover"))});a.addEventListener("mouseout",function(a){a=a.target;a.classList.contains("nuc-parent")&&(a.classList.remove("nuc-hover"),document.getElementById(a.dataset.target).previousElementSibling.classList.remove("nuc-hover"))});h(a)}}(document);</script>';
     }
 }

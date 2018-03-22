d5f708a61956b520127f741239f6589b57a5f01b
Ark4ne <gui.allegret@gmail.com>
2018-03-22 15:57:02 +0100

    feat(Version): Update to 1.3.0-ALPHA

--- src/Neutrino/Version.php
+++ src/Neutrino/Version.php
@@ -11,9 +11,9 @@ class Version extends \Phalcon\Version
     {
         return [
             1, // major
-            2, // medium
-            3, // minor
-            4, // special
+            3, // medium
+            0, // minor
+            1, // special
             0  // number
         ];
     }
